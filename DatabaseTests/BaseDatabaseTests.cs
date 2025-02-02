ed87c2a59bf72eb2f9139167a31ac78d2edf6b4c
Owen <oweningalls12@gmail.com>
2025-02-02 16:02:59 -0700

    Actually add BaseDatabaseTests

---
+++ DatabaseTests/BaseDatabaseTests.cs
@@ -0,0 +1,16 @@
+﻿using LiteDatabase;
+
+namespace DatabaseTests;
+
+public abstract class BaseDatabaseTests<T> where T : IDatabase
+{
+    protected T Database;
+    
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        Database = InitializeDatabase();
+    }
+
+    protected abstract T InitializeDatabase();
+}
